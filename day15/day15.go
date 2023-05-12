37d67a557ecf121c6f519939e5faed3622d89ebc
duketide <duketide11@gmail.com>
2023-05-12 07:55:37 -0500

    day 15 part 2

--- day15/day15.go
+++ day15/day15.go
@@ -27,7 +27,7 @@ func Day15() string {
 			tracker[num] = i
 		}
 	}
-	for len(list) < 2020 {
+	for len(list) < 30000000 {
 		idx := len(list) - 1
 		last := list[idx]
 		val, member := tracker[last]
@@ -38,5 +38,5 @@ func Day15() string {
 		tracker[last] = idx
 		list = append(list, next)
 	}
-	return "Day 15 Part 1 " + fmt.Sprint(list[2019])
+	return "Day 15 Part 1 " + fmt.Sprint(list[2019]) + " Part 2 " + fmt.Sprint(list[29999999])
 }
