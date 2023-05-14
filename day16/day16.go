53e11f8df5f2a419ef85dd724c4dc41ca0562a27
duketide <duketide11@gmail.com>
2023-05-14 10:53:39 -0500

    day 16 minor cleanup and comments

--- day16/day16.go
+++ day16/day16.go
@@ -51,6 +51,8 @@ func get_field_index(num *int, tix *[][]int, f_ranges *[][][]int, seen *[][]int,
 
 func Day16() string {
 	defer perf.Duration(perf.Track("Day16"))
+
+	//parse input
 	segments := strings.Split(strings.TrimRight(input, "\n"), "\n\n")
 	fields := parse.Lines(segments[0])
 	pfields := make([][]string, len(fields))
@@ -66,6 +68,8 @@ func Day16() string {
 	for i, f_range := range field_ranges {
 		pfield_ranges[i] = strings.Split(f_range, " or ")
 	}
+
+	//need bare_field_ranges for part 2
 	bare_field_ranges := make([][][]int, len(pfield_ranges))
 	for i, f_ranges := range pfield_ranges {
 		for _, f_range := range f_ranges {
@@ -79,15 +83,18 @@ func Day16() string {
 			bare_field_ranges[i] = append(bare_field_ranges[i], nums)
 		}
 	}
+
+	//need just_ranges for part 1
 	var just_ranges [][]int
 	for _, x := range bare_field_ranges {
 		for _, y := range x {
 			just_ranges = append(just_ranges, y)
 		}
 	}
+
+	//add my ticket to end of list of nearby tickets, convert to []int
 	my_ticket := parse.Lines(segments[1])[1:][0]
-	n_tickets := parse.Lines(segments[2])[1:]
-	n_tickets = append(n_tickets, my_ticket)
+	n_tickets := append(parse.Lines(segments[2])[1:], my_ticket)
 	s_tickets := make([][]int, len(n_tickets))
 	for i, t := range n_tickets {
 		x := strings.Split(t, ",")
@@ -99,8 +106,13 @@ func Day16() string {
 		}
 		s_tickets[i] = y
 	}
+
+	//need invalid_nums for part 1, valid_tix for part 2
 	invalid_nums := make(map[int]int)
 	var valid_tix [][]int
+
+	//main loop for part 1 (though valid_tix is only for part 2)
+	//exclude my ticket from this loop per instructions
 	for _, t := range s_tickets[:len(s_tickets)-1] {
 		invalid := false
 		for _, num := range t {
@@ -113,17 +125,32 @@ func Day16() string {
 			valid_tix = append(valid_tix, t)
 		}
 	}
-	valid_tix = append(valid_tix, s_tickets[len(s_tickets)-1])
+
+	//add my ticket back to the valid ticket list for part 2
+	my_int_ticket := s_tickets[len(s_tickets)-1]
+	valid_tix = append(valid_tix, my_int_ticket)
+
+	//sum invalid numbers to get answer for part 1
 	var p1 int
 	for k, v := range invalid_nums {
 		p1 += k * v
 	}
 	t_length := len(s_tickets[0])
+
+	//because index_in_fields will be used to determine which fields
+	//remain to be assigned, it needs to be initialized with a value
+	//that won't give bad results (e.g., -1)
 	index_in_fields := make([]int, t_length)
 	for i := range index_in_fields {
 		index_in_fields[i] = -1
 	}
+
+	//temp_seen is a mutable list of what's been tried at each position
+	//it can reset due to backtracking
 	temp_seen := make([][]int, t_length)
+
+	//perm_seen is a position_by_position list of invalid fields
+	//it should not be changed
 	perm_seen := make([][]int, t_length)
 	curr := 0
 	for curr < t_length {
@@ -137,11 +164,17 @@ func Day16() string {
 		}
 		curr++
 	}
+
+	//to optimize, we want to evaluate ticket positions from
+	//fewest # of valid possibilities to greatest
+	//this optimization changed run time from >1m to <1s
 	field_test_order := make([]int, t_length)
 	for i := range field_test_order {
 		field_test_order[i] = i
 	}
 	sort.Slice(field_test_order, func(i, j int) bool { return len(perm_seen[i]) > len(perm_seen[j]) })
+
+	//main loop for part 2
 	curr = 0
 	for curr < t_length {
 		tick_index := field_test_order[curr]
@@ -158,7 +191,8 @@ func Day16() string {
 		}
 		curr++
 	}
-	my_int_ticket := s_tickets[len(s_tickets)-1]
+
+	//get answer for part 2
 	p2 := 1
 	for i, val := range index_in_fields {
 		if val <= 5 {
