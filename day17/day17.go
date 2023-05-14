c683f6d76df499f8de19ec0ce86d2d26c6307045
duketide <duketide11@gmail.com>
2023-05-14 17:26:17 -0500

    day 17 part 2

--- day17/day17.go
+++ day17/day17.go
@@ -17,6 +17,13 @@ type coords = struct {
 	z int
 }
 
+type coords_4 = struct {
+	x int
+	y int
+	z int
+	w int
+}
+
 func get_touches(cube *coords) *[]coords {
 	result := make([]coords, 0)
 	l := []int{-1, 0, 1}
@@ -33,14 +40,34 @@ func get_touches(cube *coords) *[]coords {
 	return &result
 }
 
+func get_touches_4(cube *coords_4) *[]coords_4 {
+	result := make([]coords_4, 0)
+	l := []int{-1, 0, 1}
+	for _, h := range l {
+		for _, i := range l {
+			for _, j := range l {
+				for _, k := range l {
+					if h == 0 && i == 0 && j == 0 && k == 0 {
+						continue
+					}
+					result = append(result, coords_4{x: cube.x + i, y: cube.y + j, z: cube.z + k, w: cube.w + h})
+				}
+			}
+		}
+	}
+	return &result
+}
+
 func Day17() string {
 	defer perf.Duration(perf.Track("Day17"))
 	lines := parse.Lines(input)
 	grid := make(map[coords]bool)
+	grid_4 := make(map[coords_4]bool)
 	for x, line := range lines {
 		for y, val := range line {
 			if val == '#' {
 				grid[coords{x: x, y: y, z: 0}] = true
+				grid_4[coords_4{x: x, y: y, z: 0, w: 0}] = true
 			}
 		}
 	}
@@ -61,5 +88,22 @@ func Day17() string {
 		grid = new_grid
 		cycle++
 	}
-	return "Day 17 Part 1 " + fmt.Sprint(len(grid))
+	cycle_4 := 0
+	for cycle_4 < 6 {
+		new_grid := make(map[coords_4]bool)
+		touches := make(map[coords_4]int)
+		for cube := range grid_4 {
+			for _, touch := range *get_touches_4(&cube) {
+				touches[touch]++
+			}
+		}
+		for k, v := range touches {
+			if v == 3 || (v == 2 && set.IsMember(grid_4, k)) {
+				new_grid[k] = true
+			}
+		}
+		grid_4 = new_grid
+		cycle_4++
+	}
+	return "Day 17 Part 1 " + fmt.Sprint(len(grid)) + " Part 2 " + fmt.Sprint(len(grid_4))
 }
